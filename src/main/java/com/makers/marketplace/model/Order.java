5635d6562d1fc266db14b25acb12b3f42361e31f
SajjadMollah <sajjadmollah314@gmail.com>
2024-12-10 09:58:58 +0000

    Created Order Model

--- src/main/java/com/makers/marketplace/model/Order.java
+++ src/main/java/com/makers/marketplace/model/Order.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import java.io.Serializable;
 
 
+
+
     @Entity
     @Table(name = "orders")
     public class Order implements Serializable {
@@ -13,64 +15,55 @@ import java.io.Serializable;
         private Long orderId;
 
         @Column(unique = true, nullable = false)
-        private String username;
+        private Long sellerId;
 
-        @Column(nullable = false)
-        private String password;
+        @Column(unique = true, nullable = false)
+        private Long buyerId;
+
+
 
         @Column(nullable = false)
         private String email;
 
-        private String roles;
+
 
         // Constructors
-        public User() {}
+        public Order() {}
 
-        public User(String username, String password, String email) {
-            this.username = username;
-            this.password = password;
-            this.email = email;
+        public Order(Long sellerId, Long buyerId) {
+            this.sellerId = sellerId;
+            this.buyerId = buyerId;
         }
 
         // Getters and Setters
-        public Long getId() {
-            return id;
+        public Long getOrderId() {
+            return orderId;
         }
 
-        public void setId(Long id) {
-            this.id = id;
+        public void setOrderId(Long id) {
+            this.orderId = id;
         }
 
-        public String getUsername() {
-            return username;
+        public Long getBuyerId() {
+            return buyerId;
         }
 
-        public void setUsername(String username) {
-            this.username = username;
+        public void setBuyerId(Long buyerId) {
+            this.buyerId = buyerId;
         }
 
-        public String getPassword() {
-            return password;
+        public Long getSellerId() {
+            return sellerId;
+        }
+        public void setSellerIdd(Long sellerId) {
+            this.sellerId = sellerId;
         }
 
-        public void setPassword(String password) {
-            this.password = password;
-        }
 
-        public String getEmail() {
-            return email;
-        }
 
-        public void setEmail(String email) {
-            this.email = email;
-        }
 
-        public String getRoles() {
-            return roles;
-        }
 
-        public void setRoles(String roles) {
-            this.roles = roles;
-        }
+
+
     }
 
